90005b2c8d545c0d1632f52bdcfd3b8b83719b88
PandysActor <stage.yjhingur@droneland.nl>
2019-10-29 13:08:48 +0100

    i dunnow

--- RVO/RVO/settings.py
+++ RVO/RVO/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.landingspage.landingpageConfig',
+    'apps.map.mapConfig',
 ]
 
 MIDDLEWARE = [
